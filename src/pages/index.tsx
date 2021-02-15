59e617fbf7efa30930c521fdcafe144c7e42c6b3
NEO <neo.one.mail@gmail.com>
2021-02-15 20:24:26 +0200

    add typescript && eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: React.FC = () => (
+    <div className={styles.container}>
+        <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1>Hello World!</h1>
+    </div>
+);
+
+export default Home;
